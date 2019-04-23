112d69f4e5befb5a253050e15b80a7f41ac9b4a1
BJ Clark <bjclark13@outlook.com>
2019-04-22 22:54:57 -0400

    Add select example, clean up, add documentation

--- components/finder/finder.js
+++ components/finder/finder.js
@@ -1,28 +1,66 @@
-function Finder(PetFinder, $rootScope) {
+function Finder(PetFinder) { // Importing PetFinder service
+    // Use ctrl to affect the $ctrl values in the view
     const ctrl = this;
 
+    /**
+     * Loads in list of animals
+     * 
+     * @see Service PetFinder
+     */
     ctrl.getAnimals = () => {
-      PetFinder.getData()
+      ctrl.loading = true;
+
+      PetFinder.getData(ctrl.filter ? ctrl.filter.value : '')
       .then( (animals) => {
         ctrl.animals = animals;
+        ctrl.loading = false;
       })
     }
 
-    ctrl.value = 'this is the value';
+    /**
+     * Call types endpoint to populate select box.
+     * Lets you filter by type
+     * 
+     * @see Service PetFinder
+     */
+    ctrl.getTypes = () => {
+      PetFinder.getTypes()
+      .then( (types) => {
+        ctrl.types = types;
+      })
+      .catch( (err) => {
+        console.error(err);
+      })
+    }
 
+    /**
+     * When the app loads, call these functions to load in the data
+     */
     ctrl.$onInit = () => {
       ctrl.getAnimals();
+      ctrl.getTypes();
     }
   }
   
   angular.module('MyApp').component('finder', {
     template: `
         <h2> Pet Finder </h2>
-        <input ng-model="value" />
-        <ul>
+
+        <select 
+          ng-model="$ctrl.filter"
+          ng-options="type.name for type in $ctrl.types"
+          ng-change="$ctrl.getAnimals()"
+        >
+         <option value="">-- Choose type --</option>
+        </select>
+
+        <div ng-if="$ctrl.loading">
+          Loading...
+        </div>
+
+        <ul ng-if="!$ctrl.loading">
           <li ng-repeat="pet in $ctrl.animals track by $index"> {{ $index + 1 }}. <strong>Name:</strong>{{pet.name}} <br/> <strong>Breed:</strong>{{pet.breeds.primary}}</li>
         </ul>
-        {{$ctrl.value}}
 
     `, // or use templateUrl
     controller: Finder,
